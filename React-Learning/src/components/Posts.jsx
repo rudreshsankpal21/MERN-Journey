70c577e834fa23b50426a4fa8cc1171ed4860750
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-20 20:54:15 +0530

    Learned How to fetch data from the API using useEffect

--- React-Learning/src/components/Posts.jsx
+++ React-Learning/src/components/Posts.jsx
@@ -3,33 +3,53 @@ import axios from "axios";
 const Posts = () => {
   // useState
   const [posts, setposts] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [error, seterror] = useState(false);
   // useEffect
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((post) => {
+        setloading(false);
         // Update the posts
         setposts(post.data);
       })
-      .catch((err) => {});
-    return () => {};
-  }, []);
+      .catch((err) => {
+        seterror(err);
+        setloading(false);
+      });
+  }, [loading, error]);
+
+  // Display loading
+  if (loading) {
+    return <h1>Loading....</h1>;
+  }
+
+  // Display error
+  if (error) {
+    return <h1>Something Went Wrong</h1>;
+  }
   return (
     <>
       <h1 className="text-4xl text-center">Network Request</h1>
-      <ul>
-        {posts.map((post) => {
-          return (
-            <li className="border-2 p-4 m-4 hover:text-white hover:bg-black transition-all ease-in">
-              <h1 className="text-2xl">
-                {" "}
-                {post.id} :- {post.title}
-              </h1>
-              <p> {post.body} </p>
-            </li>
-          );
-        })}
-      </ul>
+      <div>
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li
+                className="border-2 p-4 m-4 hover:text-white hover:bg-black transition-all ease-in"
+                key={post.id}
+              >
+                <h1 className="text-2xl">
+                  {post.id} :- {post.title}
+                </h1>
+                <p> {post.body} </p>
+              </li>
+            );
+          })}
+        </ul>
+        ;
+      </div>
     </>
   );
 };
