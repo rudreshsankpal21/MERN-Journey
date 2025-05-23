44df8f367d949f150a6a13b7405a7810a281772e
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-23 21:27:53 +0530

    Practiced How to use useReducer()

--- React-Learning/src/App.jsx
+++ React-Learning/src/App.jsx
@@ -1,24 +1,24 @@
-import { useReducer } from "react";
+import React, { useReducer } from "react";
 
 const initialState = { count: 1 };
 
 const reducer = (state, action) => {
-  if (action.type === "MULBY2") {
+  if (action.type == "MULBY2") {
     return {
       ...state,
       count: state.count * 2,
     };
-  } else if (action.type === "MULBY3") {
+  } else if (action.type == "MULBY3") {
     return {
       ...state,
       count: state.count * 3,
     };
-  } else if (action.type === "MULBY4") {
+  } else if (action.type == "MULBY4") {
     return {
       ...state,
       count: state.count * 4,
     };
-  } else if (action.type === "MULBY5") {
+  } else if (action.type == "MULBY5") {
     return {
       ...state,
       count: state.count * 5,
@@ -30,15 +30,14 @@ const reducer = (state, action) => {
 
 const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
     <>
       <div>
         <h1> {state.count} </h1>
-        <button onClick={() => dispatch({ type: "MULBY2" })}>MULBY2</button>
-        <button onClick={() => dispatch({ type: "MULBY3" })}>MULBY3</button>
-        <button onClick={() => dispatch({ type: "MULBY4" })}>MULBY4</button>
-        <button onClick={() => dispatch({ type: "MULBY5" })}>MULBY5</button>
+        <button onClick={() => dispatch({ type: "MULBY2" })}>Mult by 2</button>
+        <button onClick={() => dispatch({ type: "MULBY3" })}>Mult by 3</button>
+        <button onClick={() => dispatch({ type: "MULBY4" })}>Mult by 4</button>
+        <button onClick={() => dispatch({ type: "MULBY5" })}>Mult by 5</button>
       </div>
     </>
   );
@@ -47,9 +46,7 @@ const Counter = () => {
 const App = () => {
   return (
     <>
-      <div>
-        <Counter />
-      </div>
+      <Counter />
     </>
   );
 };
