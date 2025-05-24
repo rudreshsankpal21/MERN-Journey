a6334e2e7adad1d1bfd4f593dceb19fa944bff18
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-24 16:45:07 +0530

    Practiced Form Handling

--- React-Learning/src/App.jsx
+++ React-Learning/src/App.jsx
@@ -1,59 +1,10 @@
-import React, { useReducer } from "react";
-
-const initialState = { count: 1 };
-
-const reducer = (state, action) => {
-  if (action.type == "MULBY2") {
-    return {
-      ...state,
-      count: state.count * 2,
-    };
-  } else if (action.type == "MULBY3") {
-    return {
-      ...state,
-      count: state.count * 3,
-    };
-  } else if (action.type == "MULBY4") {
-    return {
-      ...state,
-      count: state.count * 4,
-    };
-  } else if (action.type == "MULBY5") {
-    return {
-      ...state,
-      count: state.count * 5,
-    };
-  } else if (action.type == "MULBY6") {
-    return {
-      ...state,
-      count: state.count * 6,
-    };
-  } else {
-    return state;
-  }
-};
-
-const Counter = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <>
-      <div>
-        <h1> {state.count} </h1>
-        <button onClick={() => dispatch({ type: "MULBY2" })}>Mult by 2</button>
-        <button onClick={() => dispatch({ type: "MULBY3" })}>Mult by 3</button>
-        <button onClick={() => dispatch({ type: "MULBY4" })}>Mult by 4</button>
-        <button onClick={() => dispatch({ type: "MULBY5" })}>Mult by 5</button>
-        <button onClick={() => dispatch({ type: "MULBY6" })}>Mult by 6</button>
-      </div>
-    </>
-  );
-};
-
+import React from "react";
+import TeacherRegistrationForm from "./components/TeacherRegistrationForm";
 const App = () => {
   return (
-    <>
-      <Counter />
-    </>
+    <div>
+      <TeacherRegistrationForm />
+    </div>
   );
 };
 
