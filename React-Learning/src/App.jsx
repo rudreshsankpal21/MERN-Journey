2541811ff89635bfb1bc1b9057b91db58eeb257b
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-26 20:36:20 +0530

    practiced routing

--- React-Learning/src/App.jsx
+++ React-Learning/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
