efa09ae0debe7b2f137cf0c3772bd75dd2415c08
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-26 20:55:26 +0530

    initialized socket.io

---
+++ Socket.io/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const port = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new Server(server);
+
+// Listen to connection from client
+io.on("connection", (socket) => {
+  console.log(socket);
+
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+// Start the server
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
