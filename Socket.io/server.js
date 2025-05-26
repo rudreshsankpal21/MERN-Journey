86ed1646f0fe74ef70b2511d9e17cc39efc30010
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-26 21:04:30 +0530

    Integrated socket.io into the client side

--- Socket.io/server.js
+++ Socket.io/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { join } = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const server = createServer(app);
 const io = new Server(server);
 
+// Server the assets
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
 // Listen to connection from client
 io.on("connection", (socket) => {
   console.log("a user connected");
