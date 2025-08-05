6a59df6cdafa767c947df0444b57711df4d3bd72
rudresh21 <rudreshsankpal21@gmail.com>
2025-08-05 23:52:20 +0530

    feat : configured react-query & react-query-devtools

--- Redux/frontend/src/main.jsx
+++ Redux/frontend/src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store/store.js";
 
+// instance of the react-query
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
